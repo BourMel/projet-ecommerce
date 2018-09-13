9088277ceb82987aff76689256bc89407ee1d336
BourMel <meline.bl@outlook.fr>
2018-09-13 17:52:14 +0000

    static header

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "hello world<br/>";
+    
+    for($i=0; $i<10; $i++) {
+        
+        for($j=0; $j<10; $j++) {
+            echo "*";
+        }
+        echo "<br/>";
+    }
+
+?>
---
+++ templates/accueil.php
@@ -0,0 +1,15 @@
+<?php
+    require "./shared/head.html";
+    require "./shared/header.html";
+?>
+
+<main class="accueil">
+    <div class="catalogue">
+        Venez découvrir nos produits !
+        <a href="">Visitez notre catalogue</a>
+    </div>
+    
+</main>
+
+<?php    
+    require "./shared/footer.html";
