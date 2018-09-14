074e2cbbdc0f48fe9810864285c4d902cd66b03b
BourMel <meline.bl@outlook.fr>
2018-09-14 21:14:45 +0000

    static home page

--- templates/accueil.php
+++ templates/accueil.php
@@ -4,9 +4,36 @@
 ?>
 
 <main class="accueil">
-    <div class="catalogue">
-        Venez découvrir nos produits !
-        <a href="">Visitez notre catalogue</a>
+    <div class="catalogue"> 
+        <div class="wrapper d-flex justify-content-between">
+            <p>Venez découvrir nos produits !</p>
+            <a href="" class="align-self-end">Visitez notre catalogue</a>
+        </div>
+    </div>
+    
+    <span class="separation d-flex flex-column">
+        <img src="../assets/icons/heart.svg">
+        <h2>Meilleures ventes</h2>
+    </span>
+    
+    <div class="best wrapper d-flex justify-content-between">
+        <a href="" class="item">
+            <img src="../assets/images/default2.jpg">
+            <h3>Nom d'article</h3>
+            <span class="price">6,00€</span>
+        </a>
+        
+        <a href="" class="item">
+            <img src="../assets/images/default3.jpg">
+            <h3>Nom d'article</h3>
+            <span class="price">6,00€</span>
+        </a>
+        
+        <a href="" class="item">
+            <img src="../assets/images/default4.jpg">
+            <h3>Nom d'article</h3>
+            <span class="price">6,00€</span>
+        </a>
     </div>
     
 </main>
