a79db267a0d800c78f41a4303f8f3b9e0ea63d86
BourMel <meline.bl@outlook.fr>
2018-10-01 19:21:30 +0000

    small form changes

--- views/assets/javascript/form_validator.js
+++ views/assets/javascript/form_validator.js
@@ -1,5 +1,39 @@
 /**FRONT VALIDATION**/
 
+/**
+ * If msg is given, adds an error message next to an element
+ * Else, remove it
+ * Params :
+ * Id of the target element
+ * Msg to display if there is one
+ **/
+ 
+function addErrorMessage(id, msg) {
+    var $elt = $("#" + id);
+    var $parent = $elt.parent().first();
+    
+    // delete old error
+    var $oldError = $(".error." + id);
+    console.log(".error#" + id)
+    console.log($oldError);
+    
+    if($oldError.length != 0) {
+        $oldError.remove();
+    }
+    
+    // add new error
+    
+    if(msg.length > 0) {
+        var error = document.createElement("label");
+        error.classList = "error " + id;
+        error.setAttribute("for", id);
+        error.innerHTML = msg;
+        
+        $elt.after(error);
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     
     /**INSCRIPTION**/
@@ -23,28 +57,21 @@ document.addEventListener('DOMContentLoaded', function() {
             
             if(this.value.length >= 8) {
                 this.className = "valid";
+                addErrorMessage("password_insc", "");
                 
             } else {
                 this.className = "invalid";
-                // errorMsg = "Votre mot de passe n'est pas assez long (moins de 8 caractères)";
+                addErrorMessage("password_insc", "Votre mot de passe n'est pas assez long (moins de 8 caractères)");
             }
-            
-            // //if there is only numbers in the password
-            // var regex = /^[0-9]*$/;
-            // if(regex.test(this.value)) {
-                
-            // }
-            
-            // console.log(this.value)
-            // console.log(regex.test(this.value))
-                    
         });
-        document.getElementById("password_conf").addEventListener("keydown", function() {
+        
+        document.getElementById("password_conf").addEventListener("keyup", function() {
             if((this.value.length >= 8) && (this.value == password.value)) {
                 this.className = "valid";
                 
             } else {
                 this.className = "invalid";
+                addErrorMessage("password_conf", "Les mots de passe ne correspondent pas")
             }
         });
          
