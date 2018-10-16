d010d42eb39af7e61a6212e1e04bf7005b7f5038
BourMel <meline.bl@outlook.fr>
2018-10-16 17:48:47 +0000

    account_page form completed

--- views/account.php
+++ views/account.php
@@ -4,43 +4,45 @@
         <h2>Compte client</h2>
     </span>
     
-    <div class="wrapper row">
-        
-        <div class="box col-lg-4 col-sm-12">
-            <h4>Compte</h4>
+    <div class="parent_div">
+        <div class="wrapper row">
             
-            <div id="account_infos" class="display_and_form">
-                <span id="mail" data-label="Adresse email">jea_dupont@hello.net</span>
-                <span id="password" data-label="Mot de passe" data-type="password">*****</span>
+            <div class="box col-lg-4 col-sm-12">
+                <h4>Compte</h4>
+                
+                <div id="account_infos" class="display_and_form">
+                    <span id="mail" data-label="Adresse email">jea_dupont@hello.net</span>
+                    <span id="password" data-label="Mot de passe (obligatoire)" data-type="password">*****</span>
+                </div>
             </div>
-        </div>
-        
-        <div class="box col-lg-4 col-sm-12">
-            <h4>Adresse</h4>
             
-            <div id="client_infos" class="display_and_form">
-                <span id="first_name" class="inline" data-label="Prénom">Jeanne</span>
-                <span id="last_name" data-label="Nom">Dupont</span>
+            <div class="box col-lg-4 col-sm-12">
+                <h4>Adresse</h4>
                 
-                <span id="address" data-label="Adresse">7, rue de Whatever</span>
-                
-                <span id="cp" class="inline" data-label="Code postal">67000</span>
-                <span id="city" data-label="Ville">Strasbourg</span>
+                <div id="client_infos" class="display_and_form">
+                    <span id="first_name" class="inline" data-label="Prénom">Jeanne</span>
+                    <span id="last_name" data-label="Nom">Dupont</span>
+                    
+                    <span id="address" data-label="Adresse">7, rue de Whatever</span>
+                    
+                    <span id="cp" class="inline" data-label="Code postal">67000</span>
+                    <span id="city" data-label="Ville">Strasbourg</span>
+                </div>
             </div>
+            
+            <div class="box col-lg-4 col-sm-12">
+               <h4>Commandes</h4>
+               
+               <div id="command_infos">
+                    <span id="done">6 commandes réalisées</span>
+                    <span id="received">5 commandes réceptionnées</span>
+                    <span id="processing">1 commande en cours de traitement</span>
+                </div>
+            </div>
+            
         </div>
         
-        <div class="box col-lg-4 col-sm-12">
-           <h4>Commandes</h4>
-           
-           <div id="command_infos">
-                <span id="done">6 commandes réalisées</span>
-                <span id="received">5 commandes réceptionnées</span>
-                <span id="processing">1 commande en cours de traitement</span>
-            </div>
-        </div>
-        
+        <button class="set_to_form">Modifier</button>
     </div>
-    
-    <button class="set_to_form">Modifier</button>
     
 </main>
--- views/assets/javascript/display_to_form.js
+++ views/assets/javascript/display_to_form.js
@@ -3,42 +3,62 @@
  * on click of a button with a class "set_to_form"
  * the spans inside of the divs will be changed into inputs,
  * and the button to a submit button
+ * the form is added instead of a parent div with class "parent_div"
 **/
 
 document.addEventListener('DOMContentLoaded', function() {
    
+   var parentDiv = document.getElementsByClassName("parent_div");
    var divToChange = document.getElementsByClassName("display_and_form");
    var changeButton = document.getElementsByClassName("set_to_form");
    
-   if((divToChange.length >= 1) && (changeButton.length == 1)) {
-       
-       // creates all inputs
-       for(var i=0; i<divToChange.length; i++) {
-           
-           var spanToChange = divToChange[i].querySelectorAll("span");
-           
-          for(var j=0; j<spanToChange.length; j++) {
-              // we create a new input
-              var input = document.createElement("input");
-               
-              //which uses data-label and data-type attributes
-              input.setAttribute("placeholder", spanToChange[j].getAttribute("data-label"));
-              
-              if(spanToChange[j].getAttribute('data-type') == "password") {
-                  input.setAttribute("type", "password");
-              } else {
-                  input.setAttribute("type", "text");
-              }
-               
-              // and we add the new item
-              divToChange[i].appendChild(input);
-              // and remove the old one
-              divToChange[i].removeChild(spanToChange[j]);
-          }
-       }
-       
-       
-   }
+   changeButton[0].addEventListener("click", function() {
    
+       if((divToChange.length >= 1) && (changeButton.length == 1) && (parentDiv.length == 1)) {
+           
+           // creates a form inside the simple parent div
+           var form = document.createElement("form");
+           form.innerHTML = parentDiv[0].innerHTML;
+           // we remove old content
+           parentDiv[0].innerHTML = "";
+           // and replace it with the form
+           parentDiv[0].appendChild(form);
+           
+           // this elements changed so we get them again
+           divToChange = document.getElementsByClassName("display_and_form");
+           changeButton = document.getElementsByClassName("set_to_form")[0];
+           
+           // creates all inputs
+           for(var i=0; i<divToChange.length; i++) {
+               
+               var spanToChange = divToChange[i].querySelectorAll("span");
+               
+              for(var j=0; j<spanToChange.length; j++) {
+                  // we create a new input
+                  var input = document.createElement("input");
+                   
+                  //which uses data-label and data-type attributes
+                  input.setAttribute("placeholder", spanToChange[j].getAttribute("data-label"));
+                  
+                  if(spanToChange[j].getAttribute('data-type') == "password") {
+                      input.setAttribute("type", "password");
+                  } else {
+                      input.setAttribute("type", "text");
+                      // if it's not a password, we get the value and put it in the form
+                      input.setAttribute("value", spanToChange[j].innerHTML);
+                  }
+                   
+                  // and we add the new item
+                  divToChange[i].appendChild(input);
+                  // and remove the old one
+                  divToChange[i].removeChild(spanToChange[j]);
+              }
+           }
+           
+           // now we change the "change" button to a submit one
+           changeButton.innerHTML = "Valider";
+           
+       }
+   });
     
 });
